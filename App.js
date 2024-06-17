111db8b91215c740c76c4d4b6d13ef78b5125377
Hamdan Patel <hpatel164@hawk.iit.edu>
2024-06-18 00:36:00 +0530

    Fixed the no unique key bug

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading1 = React.createElement("h1", {}, "Welcome")
-const text = React.createElement("p", {}, "This is a test.....")
+const heading1 = React.createElement("h1", {key: "heading1"}, "Welcome")
+const text = React.createElement("p", {key: "text"}, "This is a test.....")
 const container = React.createElement("div", {}, [heading1, text])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
